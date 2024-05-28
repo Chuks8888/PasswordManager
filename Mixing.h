588087d60b3df4276b443dfcf0e04de383383fa1
Chuks888 <hugotarczynski@gmail.com>
2024-05-28 22:20:32 +0200

    adding changes to repo

--- Mixing.h
+++ Mixing.h
@@ -1,7 +1,9 @@
 #ifndef MIXING_H
 #define MIXING_H
 
-void LeftShift(int, unsigned char []);
-void RightShift(int, unsigned char []);
+void LeftShift(unsigned char []);
+void RightShift(unsigned char []);
+void MixColumns(unsigned char []);
+void AddRoundKey(int, unsigned char []);
 
 #endif // MIXING_H
